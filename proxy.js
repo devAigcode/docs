82c6d66981c6f1615542eefc3685282b760e66bb
devAigcode <dev@aigcode.net>
2025-05-14 19:26:33 +0800

    Documentation edits made through Mintlify web editor

--- proxy.js
+++ proxy.js
@@ -263,7 +263,7 @@ function generateUUID() {
 
 class MDX_CLASS {
     constructor() {
-        this.search = throttle(this._searchCore.bind(this), 200)
+        this.search = throttle(this._searchCore.bind(this), 50)
 
     }
     _searchCore(query) {
